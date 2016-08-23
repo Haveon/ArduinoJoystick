12f450ab5fc1270cad941f7f29dc6a5673fd4e41
Victor <victor@Nexus>
2016-08-23 11:48:37 -0400

    now waits before sending data that is zeroed

--- stick/stick.ino
+++ stick/stick.ino
@@ -1,14 +1,26 @@
+int zeroX = 515;
+int zeroY = 501;
+
 void setup() {
   Serial.begin(9600);
 }
 
 void loop() {
-  int x = analogRead(A0);
-  int y = analogRead(A1);
-  int sel = digitalRead(7);
-  Serial.print(x);
-  Serial.print(",");
-  Serial.print(y);
-  Serial.print(",");
-  Serial.println(sel);
+  // Check to see if there is a byte to read in buffer
+  if (Serial.available()>0){
+    // Remove byte from buffer
+    Serial.read();
+    
+    // Read joystick
+    int x = analogRead(A0) - zeroX;
+    int y = analogRead(A1) - zeroY;
+    int sel = !digitalRead(7);
+    
+    // Send data back on wire
+    Serial.print(x);
+    Serial.print(",");
+    Serial.print(y);
+    Serial.print(",");
+    Serial.println(sel);
+  }
 }
